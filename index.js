5cf9c0e4ea88af0c6759a55d17fbfaccda7d7174
Arfan-Dev <irfanntdc4@gmail.com>
2023-02-13 19:43:35 -0800

    my first git project through cli

---
+++ index.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let title = chalkAnimation.rainbow("Lets start calculation");
+    await sleep();
+    title.stop();
+}
+await welcome();
+async function calculator() {
+    let answers = await inquirer.prompt([
+        {
+            type: "list",
+            name: "operators",
+            choices: ["Addition", "Subtraction", "Multipication", "Division"],
+            message: "Select the operation do you want to perform"
+        },
+        {
+            type: "number",
+            name: "num1",
+            message: "Enter first number: "
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter second number: "
+        }
+    ]);
+    if (answers.operators === "Addition") {
+        console.log("Answer: " + (answers.num1 + answers.num2));
+    }
+    else if (answers.operators === "Subtraction") {
+        console.log("Answer: " + (answers.num1 - answers.num2));
+    }
+    else if (answers.operators === "Multipication") {
+        console.log("Answer: " + (answers.num1 * answers.num2));
+    }
+    else
+        console.log("Answer: " + (answers.num1 / answers.num2));
+}
+// calculator()
+async function startAgaing() {
+    do {
+        await calculator();
+        var again = await inquirer.prompt([{
+                type: "input",
+                name: "restart",
+                message: "Do you want to continue? Press y or n: "
+            }]);
+    } while (again.restart == "y" || again.restart == "yes" || again.restart == "Y" || again.restart == "YES");
+}
+startAgaing();
